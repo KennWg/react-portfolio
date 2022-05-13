d3d4a6288435542975fd2e93d9c262a987632d15
KW <kenneth_sl_wong@msn.com>
2022-05-13 14:55:31 -0400

    fixed footer styling

--- react-portfolio/src/components/Footer/index.js
+++ react-portfolio/src/components/Footer/index.js
@@ -2,12 +2,11 @@ import React from "react";
 
 function Footer() {
     return(
-        <div>
-            Kenneth Wong 2022
-            <a href="https://github.com/KennWg">GitHub</a>
-            <a href="https://www.linkedin.com/in/kenneth-wong-752829227/">LinkedIn</a>
-            <a href="https://stackoverflow.com/users/17565463/kennwg">Stack Overflow</a>
-        </div>
+        <footer className="px-2 py-2 d-flex justify-content-center">
+            <a href="https://github.com/KennWg" className="px-3" target="_blank" rel="noreferrer noopener">GitHub</a>
+            <a href="https://www.linkedin.com/in/kenneth-wong-752829227/"className="px-3" target="_blank" rel="noreferrer noopener">LinkedIn</a>
+            <a href="https://stackoverflow.com/users/17565463/kennwg"className="px-3" target="_blank" rel="noreferrer noopener">Stack Overflow</a>
+        </footer>
     );
 };
 
